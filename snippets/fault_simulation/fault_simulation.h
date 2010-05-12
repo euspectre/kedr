b3ec7d77fa5c4174f610c4190694f0e3ebf4db11
Andrey Tsyvarev <tsyvarev@ispras.ru>
2010-05-12 15:03:02 +0400

    Добавлена возможность передавать параметры для индикаторной функции при симуляции ошибочной ситуации.

--- snippets/fault_simulation/fault_simulation.h
+++ snippets/fault_simulation/fault_simulation.h
@@ -13,16 +13,35 @@
  * 
  * Returning value other than 0 means "simulate fault".
  * 
- * [NB] Precise meaning of user_data pasrameter is 
- * currently undefine.
+ * 'indicator state' points to current state of indicator.
+ * Format of state depends on indicator function.
+ * 
+ * 'user_data' points to data, passed to kedr_fsim_simulate function.
+ * Format of this data depends on simulation point.
  */
-typedef int (*fault_indicator)(void* user_data);
+
+typedef int (*kedr_fsim_fault_indicator)(void* indicator_state, void* user_data);
 
 /*
- * Type of function, which called when data is no longer needed.
+ * Format description of user_data parameter:
+ * 
+ * - "" or NULL means, that user_data parameter is not used.
+ * - "type" means, that 'user_data' points to variable of type 'type'
+ * - "type1, type2..." means, that 'user_data' points to struct
+ * 
+ *  struct{
+ *   type1 var1;
+ *   type2 var2;
+ *   ...
+ *  };
+ * 
  */
 
-typedef void (*destroy_data)(void* data);
+/*
+ * Type of function, which called when indicator is no longer needed.
+ */
+
+typedef void (*kedr_fsim_destroy_indicator_state)(void* state);
 
 /*
  * Type of point, where need to simulate.
@@ -40,15 +59,26 @@ struct kedr_simulation_point;
  * Returning value may be used in kedr_fsim_simulate
  *  and kedr_fsim_point_unregister.
  * 
+ * format_string should describe real format of user_data,
+ * which may be passed to the kedr_fsim_simulate().
+ * This format string is used to verify, whether particular indicator
+ * fits for this simulation point.
+ *
+ * It is caller who responsible for passing user_data in correct format
+ * to the kedr_fsim_simulate().
+ *
  * If this name has already used for another point, returns NULL.
  */
 
-struct kedr_simulation_point* kedr_fsim_point_register(const char* point_name);
+struct kedr_simulation_point* 
+kedr_fsim_point_register(const char* point_name,
+	const char* format_string);
 
 /*
  * Unregister point, making its name free for use.
  * 
- * This function will be automatically called when support module unload.
+ * Deregistration process is perfomed automatically
+ * when this module unload.
  */
 
 void kedr_fsim_point_unregister(struct kedr_simulation_point* point);
@@ -56,29 +86,42 @@ void kedr_fsim_point_unregister(struct kedr_simulation_point* point);
 /*
  * Invoke simulation indicator, which set for this point,
  * and return its result.
+ * 
+ * Format of 'user_data' should correspond to the format string
+ * used when point was registered.
  */
 
-int kedr_fsim_simulate(struct kedr_simulation_point* point);
+int kedr_fsim_simulate(struct kedr_simulation_point* point,
+	void* user_data);
 
 /*
- * Set fault indicator for point.
+ * Set fault indicator 'fi' for simulation point.
  * 
- * If point is not registered, return -1.
- * Otherwise set indicator fi as current indicator,
- * used by point, and return 0.
+ * 'format string' describe format of 'user_data',
+ * which indicator can process.
  *
- * Module 'm' will be locked while indicator 'fi' is set for this point.
- * 
- * 'user_data' will be passed to the indicator function,
+ * 'indicator_state' will be passed to the indicator function,
  *  when it will be called.
  * 
- * 'destroy' function will be called when user_data 
+ * 'destroy' function will be called when state 
  * will no longer been used (e.g., indicator was changed).
+ * 
+ *  When module 'm' is unloaded, indicator is cleared for this
+ *  simulation point.
+ *
+ * If point is not registered, return -1.
+ * 
+ * If indicator format string do not correspond to format string of
+ * simulation point, return 1.
+ * 
+ * Otherwise set given indicator as current indicator,
+ * used by point, and return 0.
  */
 
 int kedr_fsim_set_indicator(const char* point_name,
-	fault_indicator fi, struct module* m,
-	void* user_data, destroy_data destroy);
+	kedr_fsim_fault_indicator fi, const char* format_string,
+	struct module* m,
+	void* indicator_state, kedr_fsim_destroy_indicator_state destroy);
 
 // Additional functions may be here.
 
--- snippets/fault_simulation/fault_simulation_module.c
+++ snippets/fault_simulation/fault_simulation_module.c
@@ -11,14 +11,28 @@ MODULE_LICENSE("GPL");
 #include "module_weak_ref.h"
 #include "fault_simulation.h"
 
+/*
+ * Information about indicator, that needed for correct work of simulate
+ */
+
+struct fault_indicator_info
+{
+	kedr_fsim_fault_indicator fi;
+	struct module* m;
+	void* indicator_state;
+	kedr_fsim_destroy_indicator_state destroy;
+};
+
 struct kedr_simulation_point
 {
 	struct list_head list;
+	
 	const char* name;
-	fault_indicator fi;
-	struct module* m;
-	void* user_data;
-	destroy_data destroy;
+	//string, described format of 'user_data' parameter,
+	//taken by simulate function
+	const char* format_string;
+	
+	struct fault_indicator_info current_indicator;
 };
 // List of simulation points.
 LIST_HEAD(sim_points);
@@ -27,16 +41,28 @@ LIST_HEAD(sim_points);
 
 // Return simulation point with given name or NULL.
 struct kedr_simulation_point* fsim_lookup_point(const char* name);
-// Same as kedr_fsim_set_indicator but use point itself instead of its name
-static void fsim_set_indicator_internal(struct kedr_simulation_point* point,
-	fault_indicator fi, struct module* m,
-	void* user_data, destroy_data destroy);
 
+/*
+ *  Verify, whether data, which format is described in
+ *  'point_format_string', will be correctly interpreted by indicator,
+ *  which expect data in 'indicator_format_string' format.
+ * 
+ *  Return not 0 on success, 0 otherwise.
+ */
+
+static int
+is_data_format_compatible(	const char* point_format_string,
+							const char* indicator_format_string);
+// Same as kedr_fsim_set_indicator but use point itself instead of its name
+static int fsim_set_indicator_internal(struct kedr_simulation_point* point,
+	kedr_fsim_fault_indicator fi, const char* format_string,
+	struct module* m,
+	void* indicator_state, kedr_fsim_destroy_indicator_state destroy);
 
 // Unset indicator.
 // Called when module, provided that indicator, is unloaded
 static void unset_indicator_callback(struct module* m,
-	struct kedr_simulation_point* point);
+	struct fault_indicator_info* current_indicator);
 
 static void
 fsim_cleanup_module(void)
@@ -58,14 +84,27 @@ fsim_init_module(void)
 /*
  * Register simulation point with name 'point_name'.
  * 
+ * Initially (before calling kedr_fsim_set_indicator)
+ * point use no fault indicator, and
+ * kedr_fsim_simulate for it return always 0.
+ * 
  * Returning value may be used in kedr_fsim_simulate
  *  and kedr_fsim_point_unregister.
  * 
+ * format_string should describe real format of user_data,
+ * which may be passed to the kedr_fsim_simulate().
+ * This format string is used to verify, whether particular indicator
+ * fits for this simulation point.
+ *
+ * It is caller who responsible for passing user_data in correct format
+ * to the kedr_fsim_simulate().
+ *
  * If this name has already used for another point, returns NULL.
  */
 
 struct kedr_simulation_point* 
-kedr_fsim_point_register(const char* point_name)
+kedr_fsim_point_register(const char* point_name,
+	const char* format_string)
 {
 	struct kedr_simulation_point* new_point;
 
@@ -74,97 +113,155 @@ kedr_fsim_point_register(const char* point_name)
 	new_point = kmalloc(sizeof(*new_point), GFP_KERNEL);
 	
 	new_point->name = point_name;
-	new_point->fi = NULL;
-	new_point->m = NULL;
-	new_point->user_data = NULL;
-	new_point->destroy = NULL;
+	new_point->format_string = format_string;
+	
+	new_point->current_indicator.fi = NULL;
+	new_point->current_indicator.m = NULL;
+	new_point->current_indicator.indicator_state = NULL;
+	new_point->current_indicator.destroy = NULL;
 	
 	list_add_tail(&new_point->list, &sim_points);
 	return new_point;
 }
 EXPORT_SYMBOL(kedr_fsim_point_register);
+
 /*
  * Unregister point, making its name free for use.
  * 
- * This function will be automatically called when support module unload.
+ * Deregistration process is perfomed automatically
+ * when this module unload.
  */
 
 void kedr_fsim_point_unregister(struct kedr_simulation_point* point)
 {
-	fsim_set_indicator_internal(point, NULL, NULL, NULL, NULL);
+	fsim_set_indicator_internal(point, NULL, NULL, NULL, NULL, NULL);
 	list_del(&point->list);
 	
 	kfree(point);
 	
 }
 EXPORT_SYMBOL(kedr_fsim_point_unregister);
+
 /*
  * Invoke simulation indicator, which set for this point,
  * and return its result.
+ * 
+ * Format of 'user_data' should correspond to the format string
+ * used when point was registered.
  */
 
-int kedr_fsim_simulate(struct kedr_simulation_point* point)
+int kedr_fsim_simulate(struct kedr_simulation_point* point,
+	void* user_data)
 {
-	return point->fi ? point->fi(point->user_data) : 0;
+	return point->current_indicator.fi 
+			? point->current_indicator.fi(
+				point->current_indicator.indicator_state,
+				user_data)
+			: 0;
 }
 EXPORT_SYMBOL(kedr_fsim_simulate);
+
 /*
- * Set fault indicator for point.
+ * Set fault indicator 'fi' for simulation point.
  * 
- * If point is not registered, return -1.
- * Otherwise set indicator fi as current indicator,
- * used by point, and return 0.
+ * 'format string' describe format of 'user_data',
+ * which indicator can process.
  *
- * 'user_data' will be passed to the indicator function,
+ * 'indicator_state' will be passed to the indicator function,
  *  when it will be called.
  * 
- * 'destroy' function will be called when user_data 
+ * 'destroy' function will be called when state 
  * will no longer been used (e.g., indicator was changed).
+ * 
+ *  When module 'm' is unloaded, indicator is cleared for this
+ *  simulation point.
+ *
+ * If point is not registered, return -1.
+ * 
+ * If indicator format string do not correspond to format string of
+ * simulation point, return 1.
+ * 
+ * Otherwise set given indicator as current indicator,
+ * used by point, and return 0.
  */
 
 int kedr_fsim_set_indicator(const char* point_name,
-	fault_indicator fi, struct module* m,
-	void* user_data, destroy_data destroy)
+	kedr_fsim_fault_indicator fi, const char* format_string,
+	struct module* m,
+	void* indicator_state, kedr_fsim_destroy_indicator_state destroy)
 {
 	struct kedr_simulation_point* point = fsim_lookup_point(point_name);
 	
 	if(point == NULL) return -1;
 
-	fsim_set_indicator_internal(point, fi, m, user_data, destroy);
-	return 0;
+	return fsim_set_indicator_internal(point, fi, format_string,
+		m, indicator_state, destroy);
 }
 EXPORT_SYMBOL(kedr_fsim_set_indicator);
 
-static void fsim_set_indicator_internal(struct kedr_simulation_point* point,
-	fault_indicator fi, struct module* m,
-	void* user_data, destroy_data destroy)
+static int
+is_data_format_compatible(	const char* point_format_string,
+							const char* indicator_format_string)
 {
-	if(point->destroy)
-		point->destroy(point->user_data);
-	if(point->m)
-		module_weak_unref(point->m, 
-			(destroy_notify)unset_indicator_callback, point);
+	if(indicator_format_string == NULL
+		|| *indicator_format_string == '\0')
+	{
+		//always compatible
+		return 1;
+	}
+	else if(point_format_string == NULL
+		|| *point_format_string == '\0')
+	{
+		//no data are passed, but indicator expects something
+		return 0;
+	}
+	// simple verification, may be changed in the future
+	return strncmp(point_format_string, indicator_format_string,
+		strlen(indicator_format_string)) == 0;
+}
+
+
+static int 
+fsim_set_indicator_internal(struct kedr_simulation_point* point,
+	kedr_fsim_fault_indicator fi, const char* format_string,
+	struct module* m,
+	void* indicator_state, kedr_fsim_destroy_indicator_state destroy)
+{
+	struct fault_indicator_info* current_indicator =
+		&point->current_indicator;
+	if(!is_data_format_compatible(point->format_string,
+								format_string))
+	{
+		return 1;
+	}
 	
-	point->fi = fi;
-	point->m = m;
-	point->user_data = user_data;
-	point->destroy = destroy;
+	if(current_indicator->destroy)
+		current_indicator->destroy(current_indicator->indicator_state);
+	if(current_indicator->m)
+		module_weak_unref(current_indicator->m, 
+			(destroy_notify)unset_indicator_callback, current_indicator);
+	
+	current_indicator->fi = fi;
+	current_indicator->m = m;
+	current_indicator->indicator_state = indicator_state;
+	current_indicator->destroy = destroy;
 
 	if(m)
 		module_weak_ref(m, 
-			(destroy_notify)unset_indicator_callback, point);
+			(destroy_notify)unset_indicator_callback, current_indicator);
+	return 0;
 }
 
 static void unset_indicator_callback(struct module* m,
-	struct kedr_simulation_point* point)
+	struct fault_indicator_info* current_indicator)
 {
-	if(point->destroy)
-		point->destroy(point->user_data);
+	if(current_indicator->destroy)
+		current_indicator->destroy(current_indicator->indicator_state);
 
-	point->fi = NULL;
-	point->m = NULL;
-	point->user_data = NULL;
-	point->destroy = NULL;
+	current_indicator->fi = NULL;
+	current_indicator->m = NULL;
+	current_indicator->indicator_state = NULL;
+	current_indicator->destroy = NULL;
 	//Because it is result on module unloaded, doesn't perform weak_unref
 }
 
