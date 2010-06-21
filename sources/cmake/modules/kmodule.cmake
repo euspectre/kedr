6703abb096111062b04f895bbce4df59a2119e10
Andrey Tsyvarev <tsyvarev@ispras.ru>
2010-06-21 17:52:26 +0400

    Patch to kmodule_configure_kernel_functions macro for caching its results.

--- sources/cmake/modules/kmodule.cmake
+++ sources/cmake/modules/kmodule.cmake
@@ -40,22 +40,29 @@ endfunction(kmodule_try_compile_wout_wcflags RESULT_VAR bindir srcfile
 # kmodule_is_function_exist(function_name RESULT_VAR)
 # Verify, whether given function exist in the kernel space on the current system.
 # RESULT_VAR is TRUE, if function_name exist in the kernel space, FALSE otherwise.
+# RESULT_VAR is cached.
+
 macro(kmodule_is_function_exist function_name RESULT_VAR)
 
-    if("${RESULT_VAR}" MATCHES "^${RESULT_VAR}$")
-        message(STATUS "Looking for ${function_name} in the kernel")
+    set(kmodule_is_function_exist_message "Looking for ${function_name} in the kernel")
+    if(DEFINED ${RESULT_VAR})
+        set(kmodule_is_function_exist_message "${kmodule_is_function_exist_message} [cached]")
+    else(DEFINED ${RESULT_VAR})
         execute_process(
             COMMAND perl ${kmodule_this_module_dir}/kmodule_files/scripts/look_kernel_function.pl ${function_name}
-            RESULT_VARIABLE ${RESULT_VAR}
+            RESULT_VARIABLE kmodule_is_function_exist_result
             OUTPUT_QUIET)
-        if (${RESULT_VAR} EQUAL 0)
-            message(STATUS "Looking for ${function_name} in the kernel - found")
-            set(${RESULT_VAR} "TRUE")
-        else(${RESULT_VAR} EQUAL 0)
-            message(STATUS "Looking for ${function_name} in the kernel - not found")
-            set(${RESULT_VAR} "FALSE")
-        endif(${RESULT_VAR} EQUAL 0)
-    endif("${RESULT_VAR}" MATCHES "^${RESULT_VAR}$")
+        if (kmodule_is_function_exist_result EQUAL 0)
+            set(${RESULT_VAR} "TRUE" CACHE INTERNAL "Whether ${function_name} exist in the kernel")
+        else(kmodule_is_function_exist_result EQUAL 0)
+            set(${RESULT_VAR} "FALSE" CACHE INTERNAL "Whether ${function_name} exist in the kernel")
+        endif(kmodule_is_function_exist_result EQUAL 0)
+    endif(DEFINED ${RESULT_VAR})
+    if (${RESULT_VAR})
+        message(STATUS "${kmodule_is_function_exist_message} - found")
+    else(${RESULT_VAR})
+        message(STATUS "${kmodule_is_function_exist_message} - not found")
+    endif(${RESULT_VAR})
 
 endmacro(kmodule_is_function_exist function_name RESULT_VAR)
 #kmodule_configure_kernel_functions(output_list func1 item1 [func2 item2...])
@@ -65,9 +72,7 @@ macro(kmodule_configure_kernel_functions output_list)
 	while(kmodule_input_lists)
 		list(GET kmodule_input_lists 0 kmodule_func)
 		set(kmodule_func_varname _KMODULE_IS_${kmodule_func}_EXIST)
-		if(NOT DEFINED ${kmodule_func_varname})
-			kmodule_is_function_exist(${kmodule_func} ${kmodule_func_varname})
-		endif(NOT DEFINED ${kmodule_func_varname})
+        kmodule_is_function_exist(${kmodule_func} ${kmodule_func_varname})
 		if(${kmodule_func_varname})
 			list(GET kmodule_input_lists 1 kmodule_item)
 			list(APPEND ${output_list} ${kmodule_item})
