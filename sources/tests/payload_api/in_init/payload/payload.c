522b172a059e403d1a88b024d127d8a527094509
Eugene Shatokhin <euspectre@gmail.com>
2010-07-22 18:25:50 +0400

    Added tests for kedr_target_module_in_init()

---
+++ sources/tests/payload_api/in_init/payload/payload.c
@@ -0,0 +1,78 @@
+/*********************************************************************
+ * This module contains a replacement function for __kmalloc that checks
+ * whether the latter is called during initialization of the target module 
+ * or not. 
+ * The result is made available to user space via "target_in_init"
+ * parameter.
+ *********************************************************************/
+
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/init.h>
+
+#include <linux/slab.h>     /* __kmalloc() */
+
+#include <kedr/base/common.h>
+
+/*********************************************************************/
+MODULE_AUTHOR("Eugene");
+MODULE_LICENSE("GPL");
+/*********************************************************************/
+
+/* "target_in_init" parameter.
+ * 0 - target functon was called from module core the last time
+ * 1 - target functon was called from module init the last time
+ * 2 - target function was not called at all
+ */
+unsigned long target_in_init = 2;
+module_param(target_in_init, ulong, S_IRUGO);
+
+/*********************************************************************
+ * Replacement functions
+ *********************************************************************/
+static void*
+repl___kmalloc(size_t size, gfp_t flags)
+{
+	void* returnValue;
+    target_in_init = (kedr_target_module_in_init() ? 1 : 0);
+	returnValue = __kmalloc(size, flags);
+	return returnValue;
+}
+/*********************************************************************/
+
+/* Names and addresses of the functions of interest */
+static void* orig_addrs[] = {
+	(void*)&__kmalloc
+};
+
+/* Addresses of the replacement functions */
+static void* repl_addrs[] = {
+	(void*)&repl___kmalloc
+};
+
+static struct kedr_payload payload = {
+	.mod                    = THIS_MODULE,
+	.repl_table.orig_addrs  = &orig_addrs[0],
+	.repl_table.repl_addrs  = &repl_addrs[0],
+	.repl_table.num_addrs   = ARRAY_SIZE(orig_addrs)
+};
+/*********************************************************************/
+
+static void
+kedr_test_cleanup_module(void)
+{
+	kedr_payload_unregister(&payload);
+	return;
+}
+
+static int __init
+kedr_test_init_module(void)
+{
+	BUG_ON(	ARRAY_SIZE(orig_addrs) != 
+		ARRAY_SIZE(repl_addrs));
+	return kedr_payload_register(&payload);
+}
+
+module_init(kedr_test_init_module);
+module_exit(kedr_test_cleanup_module);
+/*********************************************************************/
---
+++ sources/tests/payload_api/in_init/target/test_in_init_target.c
@@ -0,0 +1,81 @@
+#include <linux/version.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/init.h>
+
+#include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/errno.h>
+#include <linux/spinlock.h>
+
+MODULE_AUTHOR("Eugene");
+MODULE_LICENSE("GPL");
+
+/* parameters */
+int in_init = 0;
+int in_atomic = 0;
+
+module_param(in_init, int, S_IRUGO);
+module_param(in_atomic, int, S_IRUGO);
+
+/* A spinlock to simulate an atomic context */
+spinlock_t test_lock;
+
+/* ================================================================ */
+static void
+do_test_alloc(void)
+{
+    void *ptr = NULL;
+    ptr = __kmalloc(sizeof(int), GFP_ATOMIC);
+    if (ptr == NULL)
+    {
+        printk(KERN_ALERT "[test_in_init_target] "
+            "Failed to allocate memory\n"
+        );
+        return;
+    }
+    
+    kfree(ptr);
+    return;
+}
+
+static void
+test_alloc(void)
+{
+    if (in_atomic)
+    {
+        unsigned long flags;
+        spin_lock_irqsave(&test_lock, flags);
+        do_test_alloc();
+        spin_unlock_irqrestore(&test_lock, flags);
+    }
+    else
+    {
+        do_test_alloc();
+    }
+    return;
+}
+
+static void
+kedr_test_cleanup_module(void)
+{
+    if (!in_init)
+    {
+        test_alloc();
+    }
+	return;
+}
+
+static int __init
+kedr_test_init_module(void)
+{
+    if (in_init)
+    {
+        test_alloc();
+    }
+	return 0; /* success */
+}
+
+module_init(kedr_test_init_module);
+module_exit(kedr_test_cleanup_module);
+/* ================================================================ */
