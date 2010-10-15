5210ecef07533f699985b3c9203d928ba7dd33b1
Eugene Shatokhin <euspectre@gmail.com>
2010-10-15 19:04:42 +0400

    Examples: added the tests for sample_target_2
    For now, the tests are simply copied from the original "sample_target"

---
+++ sources/examples/sample_target_2/tests/test_sample_target.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+//#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <sys/ioctl.h>
+
+#define FILENAME "/dev/cfake1"
+
+char write_buffer[135];
+void fill_write_buffer()
+{
+    int i;
+    for(i = 0; i < sizeof(write_buffer); i++)
+        write_buffer[i] = (char)(i * 2 + 3);
+}
+int main()
+{
+    int result = 0;
+    char buffer[sizeof(write_buffer) + 110];
+    
+    int fd = open(FILENAME, O_RDWR);
+    if(fd == 0)
+    {
+        printf("Failed to open file %s.\n", FILENAME);
+        return 1;
+    }
+    if(write(fd, write_buffer, sizeof(write_buffer)) != sizeof(write_buffer))
+    {
+        printf("Failed to write all bytes from write_buffer\n");
+        result = 1;
+        goto out;
+    }
+    if(lseek(fd, SEEK_SET, 0) != 0)
+    {
+        printf("Failed to position file offset to the start of the file\n");
+        result = 1;
+        goto out;
+    }
+    if(read(fd, buffer, sizeof(buffer)) < sizeof(write_buffer))
+    {
+        printf("Incorrect number of read bytes.\n");
+        result = 1;
+        goto out;
+    }
+    if(memcmp(buffer, write_buffer, sizeof(write_buffer)) != 0)
+    {
+        printf( "The bytes read from the file differ from what was "
+				"previously written to it.\n");
+        result = 1;
+        goto out;
+    }
+out:
+    close(fd);
+    return result;
+}
