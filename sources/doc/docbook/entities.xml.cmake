c17e26ab5751ed73c9eea05639c3e1134aeff965
Eugene Shatokhin <euspectre@gmail.com>
2011-06-14 17:51:35 +0400

    Set version to 0.3.0 (w/o "-devel"); set release date; regenerated HTML docs

--- sources/doc/docbook/entities.xml.cmake
+++ sources/doc/docbook/entities.xml.cmake
@@ -3,8 +3,8 @@
 <!ENTITY ispras-url "http://www.ispras.ru/en/">
 
 <!-- release- and version-related stuff -->
-<!ENTITY rel-version "@KEDR_VERSION_MAJOR@.@KEDR_VERSION_MINOR@.@KEDR_VERSION_MICRO@">
-<!ENTITY rel-date    "June 21, 2011">
+<!ENTITY rel-version "@KEDR_VERSION_MAJOR@.@KEDR_VERSION_MINOR@">
+<!ENTITY rel-date    "June 17, 2011">
 <!ENTITY kedr-lifespan   "2010-2011">
 
 <!-- email addresses -->
