0023edc23650fff60fdd187db147d071972b9ecc
Eugene Shatokhin <euspectre@gmail.com>
2012-01-20 19:20:26 +0400

    build: Made kernel function lookup script use Module.symvers if available
    lookup_kernel_function.sh will now use Module.symvers from the KBuild
    directory if available. This is more reliable than /proc/kallsyms and
    System.map because some functions that are not actually exported can still
    be marked with "T" in these files.
    
    Hit this problem on CentOS 6.2 x86-64 with alloc_pages_exact_nid() function.

--- sources/cmake/modules/kmodule_files/scripts/lookup_kernel_function.sh
+++ sources/cmake/modules/kmodule_files/scripts/lookup_kernel_function.sh
@@ -2,8 +2,26 @@
 # Usage: lookup_kernel_function.sh <function_name> [map_file]
 
 MAP_FILE=/proc/kallsyms
+SYMVERS_FILE=/lib/modules/$(uname -r)/build/Module.symvers
+SYMVERS_GREP_EXPR="[[:xdigit:]]+[[:space:]]+$1[[:space:]]+vmlinux[[:space:]]+EXPORT"
+
+# TODO: Allow specifying Module.symvers file instead of 'map_file'
+# as the argument to this script.
+
 if test -n "$2"; then
 	MAP_FILE="$2"
+else
+# Check system-wide Module.symvers first if it is present.
+# It is more reliable as Module.symvers should list only exported
+# symbols while in System.map/kallsysms internal symbols can also
+# be marked with "T".
+	if test -f "${SYMVERS_FILE}"; then
+		grep -E "${SYMVERS_GREP_EXPR}" "${SYMVERS_FILE}" > /dev/null
+		RESULT=$?
+		if test ${RESULT} -lt 2; then
+			exit ${RESULT}
+		fi
+	fi
 fi
 
 if ! test -f "${MAP_FILE}"; then
