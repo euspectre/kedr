271341b32d63bf532bfea51cc55d5280ffa611c8
Andrey Tsyvarev <tsyvarev@ispras.ru>
2014-07-30 23:16:43 +0400

    Use shell scripts for uninstall instead of cmake ones.
    This make uninstall process about 2 times faster, also symlinks are processed correctly.
    (For detect symlinks if(IS_SYMLINK) should be used, but it is absent in cmake 2.6).

--- sources/cmake/modules/uninstall_target.cmake
+++ sources/cmake/modules/uninstall_target.cmake
@@ -1,35 +1,41 @@
-# NOTE: Should be included from the top-level CMakeLists.txt.
-set(this_dir "${CMAKE_SOURCE_DIR}/cmake/modules")
+# Add 'uninstall' target to the project.
+#
+# By default, only files which has been installed are removed at 'uninstall'
+# stage.
+#
+# Function add_uninstall_dir() may be used for add directories to be
+# removed.
+get_filename_component(uninstall_target_this_dir ${CMAKE_CURRENT_LIST_FILE} PATH)
 
 configure_file(
-    "${this_dir}/uninstall_target_files/cmake_uninstall.cmake.in"
-    "${CMAKE_BINARY_DIR}/cmake_uninstall.cmake"
+    "${uninstall_target_this_dir}/uninstall_target_files/cmake_uninstall.sh.in"
+    "${CMAKE_BINARY_DIR}/cmake_uninstall.sh"
     IMMEDIATE @ONLY
 )
 
 configure_file(
-    "${this_dir}/uninstall_target_files/cmake_uninstall_dirs.cmake.in"
-    "${CMAKE_BINARY_DIR}/cmake_uninstall_dirs.cmake"
+    "${uninstall_target_this_dir}/uninstall_target_files/cmake_uninstall_dirs.sh.in"
+    "${CMAKE_BINARY_DIR}/cmake_uninstall_dirs.sh"
     IMMEDIATE @ONLY
 )
 
 
 add_custom_target (uninstall_files
-    "${CMAKE_COMMAND}" -P "${CMAKE_BINARY_DIR}/cmake_uninstall.cmake"
+    COMMAND "/bin/sh" "${CMAKE_BINARY_DIR}/cmake_uninstall.sh"
 )
 
 # Create empty directories list
-FILE(WRITE "${CMAKE_BINARY_DIR}/uninstall_dirs.txt")
+FILE(WRITE "${CMAKE_BINARY_DIR}/install_dirs_manifest.txt")
 
 # Add directory(ies) for uninstall
 function(add_uninstall_dir dir)
     foreach(d ${dir} ${ARGN})
-        FILE(APPEND "${CMAKE_BINARY_DIR}/uninstall_dirs.txt" "${d}\n")
+        FILE(APPEND "${CMAKE_BINARY_DIR}/install_dirs_manifest.txt" "${d}\n")
     endforeach(d ${dir} ${ARGN})
 endfunction(add_uninstall_dir dir)
 
 add_custom_target (uninstall_dirs
-    "${CMAKE_COMMAND}" -P "${CMAKE_BINARY_DIR}/cmake_uninstall_dirs.cmake"
+    COMMAND "/bin/sh" "${CMAKE_BINARY_DIR}/cmake_uninstall_dirs.sh"
 )
 
 
