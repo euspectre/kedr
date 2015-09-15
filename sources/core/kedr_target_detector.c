3a32c8cc84113ddf69153150919cc37ebedc2b62
Andrey Tsyvarev <tsyvarev@ispras.ru>
2015-09-15 18:56:20 +0300

    Make target detection mechanism in KEDR core to work with concurrent modules loading.
    Add test to check that payload events come in correct order.

--- sources/core/kedr_target_detector.c
+++ sources/core/kedr_target_detector.c
@@ -7,10 +7,10 @@
 /* ========================================================================
  * Copyright (C) 2012-2015, KEDR development team
  * Copyright (C) 2010-2012, Institute for System Programming 
- *                          of the Russian Academy of Sciences (ISPRAS)
+ *						  of the Russian Academy of Sciences (ISPRAS)
  * Authors: 
- *      Eugene A. Shatokhin <spectre@ispras.ru>
- *      Andrey V. Tsyvarev  <tsyvarev@ispras.ru>
+ *	  Eugene A. Shatokhin <spectre@ispras.ru>
+ *	  Andrey V. Tsyvarev  <tsyvarev@ispras.ru>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 as published
@@ -38,17 +38,19 @@
 /* Descriptor for single target. */
 struct target_struct
 {
-    /* Name of the target. */
-    char* name;
-    /* Module corresponded to given name, if loaded. Otherwise NULL. */
-    struct module* m;
+	/* Name of the target. */
+	char* name;
+	/* Module corresponded to given name, if loaded. Otherwise NULL. */
+	struct module* m;
+	/* Whether given module executes its 'init' section. */
+	int in_init;
 };
 
-/* Array of targets. */
+/* Array of targets with unique names. */
 struct target_array
 {
-    struct target_struct* arr;
-    int n;
+	struct target_struct* arr;
+	int n;
 };
 
 // Declare and initialize array of targets.
@@ -61,100 +63,148 @@ struct target_array
  * NOTE: 'm' field for the target is set to NULL.
  */
 static int target_array_add_target(struct target_array* ta,
-    const char* name, size_t name_len)
+	const char* name, size_t name_len)
 {
-    char* target_name;
-    int i;
-    struct target_struct* arr_new;
-    size_t new_size;
+	char* target_name;
+	int i;
+	struct target_struct* arr_new;
+	size_t new_size;
 
-    target_name = kmalloc(name_len + 1, GFP_KERNEL);
-    if(!target_name) return -ENOMEM;
-    
-    memcpy(target_name, name, name_len);
-    // Replace dashes with undescores
-    for(i = 0; i < name_len; i++)
-    {
-        if(target_name[i] == '-') target_name[i] = '_';
-    }
-    target_name[name_len] = '\0';
-    
-    new_size = sizeof(*arr_new) * (ta->n + 1);
-    arr_new = krealloc(ta->arr, new_size, GFP_KERNEL);
-    if(!arr_new)
-    {
-        kfree(target_name);
-        return -ENOMEM;
-    }
+	target_name = kmalloc(name_len + 1, GFP_KERNEL);
+	if(!target_name) return -ENOMEM;
+	
+	memcpy(target_name, name, name_len);
+	// Replace dashes with undescores
+	for(i = 0; i < name_len; i++)
+	{
+		if(target_name[i] == '-') target_name[i] = '_';
+	}
+	target_name[name_len] = '\0';
+
+	// Check name uniqueness.
+	for(i = 0; i < ta->n; i++)
+	{
+		if(!strcmp(target_name, ta->arr[i].name))
+		{
+			// Just ignore that addition.
+			kfree(target_name);
+			return 0;
+		}
+	}
+
+	
+	new_size = sizeof(*arr_new) * (ta->n + 1);
+	arr_new = krealloc(ta->arr, new_size, GFP_KERNEL);
+	if(!arr_new)
+	{
+		kfree(target_name);
+		return -ENOMEM;
+	}
    
-    arr_new[ta->n].name = target_name;
-    arr_new[ta->n].m = NULL;
-    
-    ta->arr = arr_new;
-    ta->n++;
-    
-    return 0;
+	arr_new[ta->n].name = target_name;
+	arr_new[ta->n].m = NULL;
+	arr_new[ta->n].in_init = 0;
+	
+	ta->arr = arr_new;
+	ta->n++;
+	
+	return 0;
 }
 
 /* Clear targets array. */
 static void target_array_clear(struct target_array* ta)
 {
-    int i;
-    for(i = 0; i < ta->n; i++)
-    {
-        kfree(ta->arr[i].name);
-    }
-    kfree(ta->arr);
-    
-    ta->arr = NULL;
-    ta->n = 0;
+	int i;
+	for(i = 0; i < ta->n; i++)
+	{
+		kfree(ta->arr[i].name);
+	}
+	kfree(ta->arr);
+	
+	ta->arr = NULL;
+	ta->n = 0;
+}
+
+/* 
+ * Move targets from `ta_src` array to `ta_dst`.
+ */
+static void target_array_move(struct target_array* ta_dst,
+	struct target_array* ta_src)
+{
+	target_array_clear(ta_dst);
+	
+	ta_dst->arr = ta_src->arr;
+	ta_dst->n = ta_src->n;
+	
+	ta_src->arr = NULL;
+	ta_src->n = 0;
 }
 
 /* Global array of targets */
 static TARGET_ARRAY(targets);
 
-/* Protect 'targets' array  from concurrent access. */
+/* Whether module notifier is registered. 
+ * 
+ * Until module notifier is registered, it is unreliable to check
+ * whether module with given name is loaded.
+ * 
+ * This variable is set only during initialization, so it does not
+ * require locking rules: interfaces are not allowed to be used *during*
+ * initialization.
+ */
+int is_module_notifier_registered = 0;
+
+/* Global array of targets *for being set*. */
+static TARGET_ARRAY(targets_pending);
+
+/* 
+ * Protect 'targets' and `targets_pending` arrays from concurrent
+ * access.
+ */
 static DEFINE_MUTEX(target_mutex);
+
+/* Counter of targets in init section. */
+static atomic_t target_init_counter = ATOMIC_INIT(0);
 
 /**************** Writer into buffer of limited length ****************/
 struct buffer_writer
 {
-    char* p;
-    int max_size;
-    int size;
+	char* p;
+	int max_size;
+	int size;
 };
 
 #define BUFFER_WRITER(name, p, max_size) struct buffer_writer name = {p, max_size, 0}
 
 /* Write 'bytes' array of length 'n' into buffer. Exceeded characters are discarded. */
 static inline void buffer_write_bytes(struct buffer_writer* bw,
-    const char* bytes, int n)
+	const char* bytes, int n)
 {
-    if(bw->max_size != bw->size)
-    {
-        int n_real = bw->size + n < bw->max_size ? n : bw->max_size - bw->size;
-        memcpy(bw->p + bw->size, bytes, n_real);
-        bw->size += n_real;
-    }
+	if(bw->max_size != bw->size)
+	{
+		int n_real = bw->size + n < bw->max_size ? n : bw->max_size - bw->size;
+		memcpy(bw->p + bw->size, bytes, n_real);
+		bw->size += n_real;
+	}
 }
 
 /* Write character into buffer. If buffer is full, do nothing. */
 static inline void buffer_write_char(struct buffer_writer* bw, char c)
 {
-    if(bw->max_size != bw->size)
-    {
-        bw->p[bw->size] = c;
-        bw->size++;
-    }
+	if(bw->max_size != bw->size)
+	{
+		bw->p[bw->size] = c;
+		bw->size++;
+	}
 }
 
 /*********** Implementation of 'target' module parameter **************/
 int kedr_target_detector_get_target_name(char* buf, size_t size)
 {
-    int i;
-    BUFFER_WRITER(bw, buf, size);
-    
-    mutex_lock(&target_mutex);
+	int i;
+	BUFFER_WRITER(bw, buf, size);
+	
+	mutex_lock(&target_mutex);
 
 	for(i = 0; i < targets.n; i++)
 	{
@@ -168,174 +218,189 @@ int kedr_target_detector_get_target_name(char* buf, size_t size)
 		
 		buffer_write_bytes(&bw, name, len);
 	}
-    
-    mutex_unlock(&target_mutex);
-    
-    return bw.size;
+	
+	mutex_unlock(&target_mutex);
+	
+	return bw.size;
+}
+
+/* 
+ * Set 'targets' according to value of 'targets_pending' with checks.
+ * 
+ * Can be called only when 'is_module_notifier_registered' is true.
+ * 
+ * Should be called with 'target_mutex' locked.
+ */
+static int set_target_name_internal(void)
+{
+	int i;
+	int result = 0;
+	BUG_ON(!is_module_notifier_registered);
+	
+	for(i = 0; i < targets.n; i++)
+	{
+		if(targets.arr[i].m)
+		{
+			kedr_err0("Cannot change target while it is loaded");
+			return -EBUSY;
+		}
+	}
+	
+	mutex_lock(&module_mutex); // Need for find_module() call.
+	for(i = 0; i < targets_pending.n; i++)
+	{
+		const char* target_name = targets_pending.arr[i].name;
+		
+		if(find_module(target_name))
+		{
+			kedr_err("Cannot add target name '%s' because corresponded module is currently loaded.",
+				target_name);
+			result = -EINVAL;
+			break;
+		}
+		
+	}
+	mutex_unlock(&module_mutex);
+	if(result) return result;
+		
+	if(targets_pending.n > 1)
+	{
+		result = force_several_targets();
+		if(result) return result;
+	}
+	else
+	{
+		unforce_several_targets();
+	}
+	
+	target_array_move(&targets, &targets_pending);
+
+	return 0;
 }
 
 int kedr_target_detector_set_target_name(const char* target_name)
 {
-    int result = 0;
-	int i;
-    /* 
-     * Temporary array of targets.
-     * It will be assigned to global one after being checked.
-     */
-    TARGET_ARRAY(targets_new);
-    
-    int can_use_locks = param_set_can_use_lock();
-    
-    if(can_use_locks) mutex_lock(&target_mutex);
-    
-    for(i = 0; i < targets.n; i++)
-    {
-		if(targets.arr[i].m)
+	int result = 0;
+	static const char* delims = ",;\n";
+	const char* beg = target_name;
+
+	int can_use_locks = param_set_can_use_lock();
+
+	if(can_use_locks) mutex_lock(&target_mutex);
+
+	/* Unconditionally replace 'targets_pending' array. */
+	target_array_clear(&targets_pending);
+
+	while(*beg)
+	{
+		size_t len = strcspn(beg, delims);
+		if(len)
 		{
-			kedr_err0("Cannot change target while it is loaded");
-			result = -EBUSY;
-			goto fail;
+			result = target_array_add_target(&targets_pending, beg, len);
+			if(result) goto fail;
 		}
+
+		beg += len;
+		if(*beg) beg++;
 	}
-    
-    {
-        static const char* delims = ",;\n";
-        const char* beg = target_name;
-        
-        mutex_lock(&module_mutex);
-        while(*beg)
-        {
-            size_t len = strcspn(beg, delims);
-            if(len)
-            {
-                result = target_array_add_target(&targets_new, beg, len);
-                if(result) break;
-                
-                if(find_module(targets_new.arr[targets_new.n - 1].name))
-                {
-                    kedr_err("Cannot add target name '%s' because corresponded module is currently loaded.",
-                        targets_new.arr[targets_new.n - 1].name);
-                    result = -EINVAL;
-                    break;
-                }
-            }
-            
-            beg += len;
-            if(*beg) beg++;
-        }
 
-        mutex_unlock(&module_mutex);
+	if(is_module_notifier_registered)
+	{
+		result = set_target_name_internal();
+		if(result) goto fail;
+	}
 
-        if(result) goto fail;
-        
-        if(targets_new.n > 1)
-        {
-            result = force_several_targets();
-            if(result) goto fail;
-        }
-        else
-        {
-            unforce_several_targets();
-        }
-    }
-
-    // Set global 'targets' array
-    target_array_clear(&targets);
-    
-    targets.arr = targets_new.arr;
-    targets.n = targets_new.n;
-
-    if(can_use_locks) mutex_unlock(&target_mutex);
-    return 0;
+	if(can_use_locks) mutex_unlock(&target_mutex);
+	return 0;
 
 fail:
-    if(can_use_locks) mutex_unlock(&target_mutex);
+	target_array_clear(&targets_pending);
 
-    target_array_clear(&targets_new);
+	if(can_use_locks) mutex_unlock(&target_mutex);
 
-    return result;
+	return result;
 }
 
 /* ================================================================== */
-/* 
- * Whether there is an target which is currently being initialized.
- * 
- * Because of Linux kernel's implementation, at most one target module
- * may have that property.
- */
-int target_in_init = 0;
 
 int kedr_target_module_in_init(void)
 {
-    return target_in_init;
+	return atomic_read(&target_init_counter) != 0;
 }
 
 /* ================================================================== */
 /* 
  * A callback function to catch loading and unloading of module.
  * 
- * Sets 'target_in_init' among other things.
+ * Update 'target_init_counter' among other things.
  */
 static int 
 detector_notifier_call(struct notifier_block *nb,
 	unsigned long mod_state, void *vmod)
 {
 	struct module* mod = (struct module *)vmod;
-    int i;
+	struct target_struct* target = NULL;
+	int i;
 	BUG_ON(mod == NULL);
 	
+	mutex_lock(&target_mutex);
+
+	for(i = 0; i < targets.n; i++)
+	{
+		target = &targets.arr[i];
+		
+		/*
+		 * Transitions of *already loaded* target are not tracked,
+		 * if its loading hasn't be detected.
+		 * 
+		 * NOTE: In the current KEDR core implementation such situation
+		 * is impossible.
+		 * 
+		 * Also this filter is cheaper than strncmp() one in case
+		 * of non-target module is unloaded.
+		 */
+		if(!target->m && (mod_state != MODULE_STATE_COMING)) continue;
+		
+		if(!strncmp(module_name(mod), target->name, MODULE_NAME_LEN))
+			break;
+	}
+	
+	if(i == targets.n) goto out; // Not a target.
+
 	/* handle module state change */
 	switch(mod_state)
 	{
 	case MODULE_STATE_COMING: /* the module has just loaded */
-		mutex_lock(&target_mutex);
-        
-        for(i = 0; i < targets.n; i++)
-        {
-            if(targets.arr[i].m) continue;
-            
-            if(!strncmp(module_name(mod), targets.arr[i].name, MODULE_NAME_LEN))
-            {
-                if(!on_target_load(mod))
-                {
-                    targets.arr[i].m = mod;
-                    target_in_init = 1;
-                }
-                break;
-            }
-        }
-        
-		mutex_unlock(&target_mutex);
+		if(on_target_load(mod)) goto out;
+		target->m = mod;
+		target->in_init = 1;
+		atomic_inc(&target_init_counter);
 	break;
-	case MODULE_STATE_LIVE:
-        /* 
-         * At most one module may leave initialization stage.
-         * So, it is safe to reset 'target_in_init' unconditionally.
-         */
-        target_in_init = 0;
-    break;
+	case MODULE_STATE_LIVE: /* the module has just initialized */
+		target->in_init = 0;
+		atomic_dec(&target_init_counter);
+	break;
 	case MODULE_STATE_GOING: /* the module is going to unload */
-		mutex_lock(&target_mutex);
-        for(i = 0; i < targets.n; i++)
-        {
-            if(mod == targets.arr[i].m)
-            {
-                on_target_unload(mod);
-                targets.arr[i].m = NULL;
-                /* 
-                 * If module initialization fails, no MODULE_STATE_LIVE
-                 * notification will be emited.
-                 * So, reset 'target_in_init' here.
-                 */
-                target_in_init = 0;
-                break;
-            }
-        }
-
-		mutex_unlock(&target_mutex);
+		/* 
+		 * This state can come just after initialization
+		 * (e.g. if that initialization fails), so care with
+		 * 'target_init_counter'.
+		 * 
+		 * NOTE: This is the only reason for 'in_init' field in
+		 * 'target_struct'.
+		 */
+		on_target_unload(mod);
+		if(target->in_init)
+		{
+			target->in_init = 0;
+			atomic_dec(&target_init_counter);
+		}
+		target->m = NULL;
 	break;
 	}
 
+out:
+	mutex_unlock(&target_mutex);
 	return 0;
 }
 
@@ -359,16 +424,25 @@ kedr_target_detector_init(void)
 
 	result = register_module_notifier(&detector_nb);
 	
-	if(result)
-	{
-		/*
-		 * Targets array may be set before initialization.
-		 * Clear it in case of error.
-		 */
-		target_array_clear(&targets);
+	if(result) goto err;
 
-	}
+	mutex_lock(&target_mutex);
+	is_module_notifier_registered = 1;
+	result = set_target_name_internal();
+	mutex_unlock(&target_mutex);
 
+	if(result) goto err_set_target;
+	
+	return 0;
+
+err_set_target:
+	unregister_module_notifier(&detector_nb);
+err:
+	/*
+	 * Pending targets array may be set before initialization.
+	 * Clear it in the case of error.
+	 */
+	target_array_clear(&targets_pending);
 	return result;
 }
 
@@ -377,5 +451,7 @@ kedr_target_detector_destroy(void)
 {
 	unregister_module_notifier(&detector_nb);
 
-    target_array_clear(&targets);
+	target_array_clear(&targets);
+	// Just in case
+	target_array_clear(&targets_pending);
 }
