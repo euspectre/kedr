506a0b3f0c7010bcd460405973c48a3436e8508c
Evgenii Shatokhin <eugene.shatokhin@rosalab.ru>
2017-04-16 22:25:51 +0300

    core: implemented setting and removing function handlers via Ftrace
    The ideas and part of the code are borrowed from the implementation of
    livepatch in the mainline kernel.
    
    The instrumented parts of the kernel contain the calls to the special stubs.
    The stubs do nothing by themselves but can be redirected to the real
    handlers with the help of Ftrace.
    
    When the core module (kedr.ko) is loaded, it finds the stubs by looking
    through kallsyms and uses Ftrace to "replace" them with real handlers
    that it provides. When the core module is unloaded, the handlers are
    detached and the instrumented code will continue working, calling the
    stubs that do nothing, etc.

--- kernel/arch/x86/include/asm/kedr.h
+++ kernel/arch/x86/include/asm/kedr.h
@@ -1,6 +1,13 @@
+/* similar to arch/x86/include/asm/livepatch.h */
+
 #ifndef _ASM_X86_KEDR_H
 #define _ASM_X86_KEDR_H
 
-/* arch-specific decls */
+#include <linux/ftrace.h>
+
+static inline void kedr_arch_set_pc(struct pt_regs *regs, unsigned long ip)
+{
+	regs->ip = ip;
+}
 
 #endif
--- kernel/kernel/kedr/core.c
+++ kernel/kernel/kedr/core.c
@@ -6,6 +6,11 @@
  * by the Free Software Foundation.
  ======================================================================== */
 
+/*
+ * Some parts of this code may be based on the implementation of livepatch
+ * in the mainline kernel.
+ */
+
 #include <linux/version.h>
 #include <linux/module.h>
 #include <linux/init.h>
@@ -33,6 +38,11 @@ MODULE_AUTHOR("Evgenii Shatokhin");
 MODULE_LICENSE("GPL");
 /* ====================================================================== */
 
+enum kedr_func_state {
+	KEDR_FUNC_DISABLED,
+	KEDR_FUNC_ENABLED
+};
+
 struct kedr_object {
 	struct list_head list;
 	struct module *mod;	/* NULL for vmlinux, non-NULL for modules */
@@ -48,6 +58,11 @@ struct kedr_func {
 
 	/* Address of the stub the handler is attached to. */
 	unsigned long addr;
+
+	/* Information about the function: for messages, etc. */
+	char *info;
+
+	enum kedr_func_state state;
 };
 /* ====================================================================== */
 
@@ -62,29 +77,258 @@ static LIST_HEAD(kedr_objects);
 static bool kedr_enabled;
 /* ====================================================================== */
 
-/* The handlers. */
+/*
+ * The handlers.
+ * Preemption is disabled there, that allows us to use synchronize_sched()
+ * later to wait for all running handlers to complete.
+ */
 static void alloc_pre(struct kedr_local *local)
 {
+	preempt_disable();
 	// TODO
+	//<>
+	//pr_err("[DBG] alloc_pre at %pS: size = %lu\n",
+		(void *)local->pc, local->size);
+	//<>
+	preempt_enable();
 }
 
 static void alloc_post(struct kedr_local *local)
 {
+	preempt_disable();
 	// TODO
+	//<>
+	//pr_err("[DBG] alloc_post at %pS: size = %lu, addr = %p\n",
+		(void *)local->pc, local->size, (void *)local->addr);
+	//<>
+	preempt_enable();
 }
 
 static void free_pre(struct kedr_local *local)
 {
+	preempt_disable();
 	// TODO
+	//<>
+	//pr_err("[DBG] free_pre at %pS: addr = %p\n",
+		(void *)local->pc, (void *)local->addr);
+	//<>
+	preempt_enable();
 }
 
 static void free_post(struct kedr_local *local)
 {
+	preempt_disable();
 	// TODO
+	//<>
+	//pr_err("[DBG] free_post at %pS: addr = %p\n",
+		(void *)local->pc, (void *)local->addr);
+	//<>
+	preempt_enable();
 }
 /* ====================================================================== */
 
+static void notrace kedr_ftrace_handler(unsigned long ip,
+				       unsigned long parent_ip,
+				       struct ftrace_ops *fops,
+				       struct pt_regs *regs)
+{
+	struct kedr_func *func;
 
+	func = container_of(fops, struct kedr_func, ops);
+	kedr_arch_set_pc(regs, (unsigned long)func->handler);
+}
+
+/* ====================================================================== */
+
+/* Note. mod == NULL corresponds to the kernel proper here. */
+static struct kedr_object *kedr_find_object(struct module *mod)
+{
+	struct kedr_object *obj;
+
+	list_for_each_entry(obj, &kedr_objects, list) {
+		if (obj->mod == mod)
+			return obj;
+	}
+	return NULL;
+}
+
+/* Note. We assume here that the object for 'mod' does not exist yet. */
+static struct kedr_object *kedr_create_object(struct module *mod)
+{
+	struct kedr_object *obj;
+
+	obj = kzalloc(sizeof(*obj), GFP_KERNEL);
+	if (obj) {
+		obj->mod = mod;
+		INIT_LIST_HEAD(&obj->funcs);
+		list_add(&obj->list, &kedr_objects);
+	}
+	return obj;
+}
+
+static void kedr_destroy_func(struct kedr_func *func)
+{
+	if (!func)
+		return;
+	kfree(func->info);
+	kfree(func);
+}
+
+static struct kedr_func *kedr_create_func(
+	void (*handler)(struct kedr_local *local),
+	unsigned long addr,
+	const char *name,
+	const char *module_name)
+{
+	struct kedr_func *func;
+	int len;
+	static const char *fmt = "%s at %p (%s)";
+
+	func = kzalloc(sizeof(*func), GFP_KERNEL);
+	if (!func)
+		return NULL;
+
+	len = snprintf(NULL, 0, fmt, name, (void *)addr, module_name) + 1;
+	func->info = kzalloc(len, GFP_KERNEL);
+	if (!func->info) {
+		kfree(func);
+		return NULL;
+	}
+	snprintf(func->info, len, fmt, name, (void *)addr, module_name);
+
+	func->ops.func = kedr_ftrace_handler;
+	func->ops.flags = FTRACE_OPS_FL_SAVE_REGS |
+			  FTRACE_OPS_FL_DYNAMIC |
+			  FTRACE_OPS_FL_IPMODIFY;
+	func->handler = handler;
+	func->addr = addr;
+	func->state = KEDR_FUNC_DISABLED;
+	return func;
+}
+
+/*
+ * Convert a function address into the appropriate ftrace location.
+ *
+ * Usually this is just the address of the function, but on some architectures
+ * it's more complicated so allow them to provide a custom behaviour.
+ */
+#ifndef kedr_get_ftrace_location
+static unsigned long kedr_get_ftrace_location(unsigned long faddr)
+{
+	return faddr;
+}
+#endif
+
+static void kedr_func_detach(struct kedr_func *func)
+{
+	unsigned long ftrace_loc;
+	int ret;
+
+	if (func->state != KEDR_FUNC_ENABLED) {
+		pr_info(KEDR_PREFIX
+			"Handler for the function %s is not enabled.\n",
+			func->info);
+		return;
+	}
+
+	ftrace_loc = kedr_get_ftrace_location(func->addr);
+	if (!ftrace_loc) {
+		pr_err(KEDR_PREFIX
+			"Failed to find ftrace hook for the function %s\n",
+			func->info);
+		return;
+	}
+
+	ret = unregister_ftrace_function(&func->ops);
+	if (ret) {
+		pr_err(KEDR_PREFIX
+			"Failed to unregister ftrace handler for function %s (error: %d)\n",
+			func->info, ret);
+		return;
+	}
+
+	ret = ftrace_set_filter_ip(&func->ops, ftrace_loc, 1, 0);
+	if (ret) {
+		pr_err(KEDR_PREFIX
+			"Failed to remove ftrace filter for function %s (error: %d)\n",
+			func->info, ret);
+		return;
+	}
+}
+
+static int kedr_func_attach(struct kedr_func *func)
+{
+	unsigned long ftrace_loc;
+	int ret;
+
+	if (func->state != KEDR_FUNC_DISABLED) {
+		pr_err(KEDR_PREFIX
+		       "Handler for the function %s is already enabled.\n",
+		       func->info);
+		return -EINVAL;
+	}
+
+	ftrace_loc = kedr_get_ftrace_location(func->addr);
+	if (!ftrace_loc) {
+		pr_err(KEDR_PREFIX
+			"Failed to find ftrace hook for the function %s\n",
+			func->info);
+		return -EINVAL;
+	}
+
+	ret = ftrace_set_filter_ip(&func->ops, ftrace_loc, 0, 0);
+	if (ret) {
+		pr_err(KEDR_PREFIX
+			"Failed to set ftrace filter for function %s (error: %d)\n",
+			func->info, ret);
+		return -EINVAL;
+	}
+
+	ret = register_ftrace_function(&func->ops);
+	if (ret) {
+		pr_err(KEDR_PREFIX
+			"Failed to register ftrace handler for function %s (error: %d)\n",
+			func->info, ret);
+		ftrace_set_filter_ip(&func->ops, ftrace_loc, 1, 0);
+		return -EINVAL;
+	}
+
+	func->state = KEDR_FUNC_ENABLED;
+
+	//<>
+	pr_warning("[DBG] Attached handler to the function %s.\n", func->info);
+	//<>
+	return 0;
+}
+
+/*
+ * Detaches all handlers attached via this object, if any. Frees all the
+ * memory allocated for the respective kedr_func instances.
+ * Does not free the object itself.
+ */
+static void kedr_cleanup_object(struct kedr_object *obj)
+{
+	struct kedr_func *func;
+	struct kedr_func *tmp;
+
+	list_for_each_entry_safe(func, tmp, &obj->funcs, list) {
+		kedr_func_detach(func);
+		list_del(&func->list);
+		kedr_destroy_func(func);
+	}
+}
+
+static void kedr_destroy_all_objects(void)
+{
+	struct kedr_object *obj;
+	struct kedr_object *tmp;
+
+	list_for_each_entry_safe(obj, tmp, &kedr_objects, list) {
+		kedr_cleanup_object(obj);
+		list_del(&obj->list);
+		kfree(obj);
+	}
+}
 
 static int kedr_kallsyms_callback(void *data, const char *name,
 				  struct module *mod, unsigned long addr)
@@ -93,6 +337,9 @@ static int kedr_kallsyms_callback(void *data, const char *name,
 	static size_t prefix_len = ARRAY_SIZE(stub_prefix) - 1;
 	struct module *target = data;
 	const char *part;
+	struct kedr_object *obj;
+	void (*handler)(struct kedr_local *local);
+	struct kedr_func *func;
 
 	/*
 	 * If 'target' is NULL, we need to check all the symbols.
@@ -107,24 +354,16 @@ static int kedr_kallsyms_callback(void *data, const char *name,
 
 	part = name + prefix_len;
 	if (strcmp(part, "alloc_pre") == 0) {
-		// TODO
-		pr_info(KEDR_PREFIX "Got alloc_pre() in %s at %p.\n",
-			module_name(mod), (void *)addr);
+		handler = alloc_pre;
 	}
 	else if (strcmp(part, "alloc_post") == 0) {
-		// TODO
-		pr_info(KEDR_PREFIX "Got alloc_post() in %s at %p.\n",
-			module_name(mod), (void *)addr);
+		handler = alloc_post;
 	}
 	else if (strcmp(part, "free_pre") == 0) {
-		// TODO
-		pr_info(KEDR_PREFIX "Got free_pre() in %s at %p.\n",
-			module_name(mod), (void *)addr);
+		handler = free_pre;
 	}
 	else if (strcmp(part, "free_post") == 0) {
-		// TODO
-		pr_info(KEDR_PREFIX "Got free_post() in %s at %p.\n",
-			module_name(mod), (void *)addr);
+		handler = free_post;
 	}
 	else {
 		pr_info(KEDR_PREFIX "Unknown KEDR stub \"%s\" in %s.\n",
@@ -132,9 +371,55 @@ static int kedr_kallsyms_callback(void *data, const char *name,
 		return 0;
 	}
 
-	// TODO: attach the handler. Keep the state (successfully attached or not) in the structure?
-	// Or just create the structures and attach the handlers later?
+	obj = kedr_find_object(mod);
+	if (!obj) {
+		obj = kedr_create_object(mod);
+		if (!obj)
+			return -ENOMEM;
+	}
+
+	func = kedr_create_func(handler, addr, name, module_name(mod));
+	if (!func)
+		return -ENOMEM;
+
+	list_add(&func->list, &obj->funcs);
 	return 0;
+}
+
+/*
+ * Detach the handlers from the KEDR stubs in the given module (if mod is
+ * not NULL) or everywhere (if mod is NULL).
+ */
+static void kedr_detach_handlers(struct module *mod)
+{
+	if (mod) {
+		struct kedr_object *obj;
+
+		obj = kedr_find_object(mod);
+		if (obj) {
+			kedr_cleanup_object(obj);
+			list_del(&obj->list);
+			kfree(obj);
+		}
+	}
+	else {
+		kedr_destroy_all_objects();
+	}
+}
+
+static int kedr_attach_all_for_object(struct kedr_object *obj)
+{
+	struct kedr_func *func;
+	int ret = 0;
+
+	list_for_each_entry(func, &obj->funcs, list) {
+		if (func->state != KEDR_FUNC_DISABLED)
+			continue;
+		ret = kedr_func_attach(func);
+		if (ret)
+			break;
+	}
+	return ret;
 }
 
 /*
@@ -145,22 +430,39 @@ static int kedr_kallsyms_callback(void *data, const char *name,
 static int kedr_attach_handlers(struct module *mod)
 {
 	int ret;
+	struct kedr_object *obj;
 
 	ret = mutex_lock_killable(&module_mutex);
 	if (ret)
 		return ret;
-	kallsyms_on_each_symbol(kedr_kallsyms_callback, mod);
-	mutex_unlock(&module_mutex);
-	return 0;
-}
 
-/*
- * Detach the handlers from the KEDR stubs in the given module (if mod is
- * not NULL) or everywhere (if mod is NULL).
- */
-static void kedr_detach_handlers(struct module *mod)
-{
-	// TODO
+	ret = kallsyms_on_each_symbol(kedr_kallsyms_callback, mod);
+	mutex_unlock(&module_mutex);
+	if (ret)
+		return ret;
+
+	/*
+	 * Ftrace code may lock module_mutex too, e.g., when calling
+	 * set_all_modules_text_rw(), so we cannot attach the handlers
+	 * in the kallsyms callback itself. Do it here instead.
+	 */
+	if (mod) {
+		obj = kedr_find_object(mod);
+		if (obj)
+			ret = kedr_attach_all_for_object(obj);
+	}
+	else {
+		list_for_each_entry(obj, &kedr_objects, list) {
+			ret = kedr_attach_all_for_object(obj);
+			if (ret)
+				break;
+		}
+	}
+
+	if (ret)
+		kedr_detach_handlers(mod);
+
+	return ret;
 }
 /* ====================================================================== */
 
@@ -182,8 +484,11 @@ static int kedr_module_notify(struct notifier_block *nb, unsigned long action,
 			pr_warning(KEDR_PREFIX "Failed to lock kedr_mutex.\n");
 			break;
 		}
-		if (kedr_enabled)
-			ret = kedr_attach_handlers(mod);
+		if (!kedr_enabled) {
+			mutex_unlock(&kedr_mutex);
+			break;
+		}
+		ret = kedr_attach_handlers(mod);
 		if (ret)
 			pr_warning(
 				KEDR_PREFIX
