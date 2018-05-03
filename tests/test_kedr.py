ef70f204003508586472490695f9d749e4c5ff0c
Evgenii Shatokhin <eugene.shatokhin@rosalab.ru>
2018-05-03 22:37:39 +0300

    tests: Refactoring
    Actually, I replaced the simple test with a better structured one, to
    facilitate adding new tests.
    
    The test case classes should now inherit KedrTest that implements common
    operations. The parameters (kernel version, source/build directories) are
    passed to the tests via class variables.
    
    Environment variables are no longer used to pass these data to the test
    script. Command-line parameters are provided instead.
    
    It is now up to the test, how to find the rules file and which such file
    to use.

---
+++ tests/test_kedr.py
@@ -0,0 +1,348 @@
+#!/usr/bin/env python3
+
+''' The tests for KEDR.
+
+Usage:
+    test_kedr.py <name_of_test_suite>
+'''
+
+import sys
+import os
+import os.path
+import shutil
+import subprocess
+import unittest
+import time
+import argparse
+
+
+CORE_SUBDIR = 'core'
+KEDR_CORE = CORE_SUBDIR + '/kernel/kedr/kedr.ko'
+KEDR_ENABLED = '/sys/kernel/kedr/enabled'
+
+def build_kedr_core(topsrcdir, test_dir, kernel):
+    '''Build KEDR core for the given kernel.'''
+    print('Building the KEDR core module.')
+    shutil.copytree(os.path.join(topsrcdir, 'kernel'),
+                    os.path.join(test_dir, CORE_SUBDIR))
+    subprocess.run(['make',
+                    'KBUILD_DIR=/lib/modules/%s/build/' % kernel,
+                    '-C', CORE_SUBDIR],
+                   check=True)
+    if not os.path.exists(KEDR_CORE):
+        raise RuntimeError(KEDR_CORE + ' is missing.')
+
+    print('Successfully built ' + os.path.join(test_dir, KEDR_CORE))
+
+
+class KedrTest(unittest.TestCase):
+    '''The common base class for the tests.'''
+
+    # Class variables allow making parameterized test cases.
+    kernel = None
+    testdir = '.'
+    topsrcdir = '.'
+    topbuilddir = '.'
+
+    def __init__(self, methodName='runTest'):
+        unittest.TestCase.__init__(self, methodName)
+        self.last_dmesg_line = None
+
+    @staticmethod
+    def build_target_module(target_dir, target_mod, kernel, rules):
+        '''Build the target module.
+
+         The function builds the target module for the given kernel using
+         the instrumentation rules from the specified file.
+
+         target_dir - directory with the sources of the target module;
+         target_mod - name of the target module ('<something>.ko');
+         rules - path to the file with the instrumentation rules.
+
+         The function returns True if the module has been built
+         successfully, False otherwise.
+        '''
+        print('Building the target kernel module: %s.' % target_mod)
+
+        env = os.environ
+        env['KEDR_RULES_FILE'] = rules
+        proc = subprocess.Popen(
+            ['make',
+             'KBUILD_DIR=/lib/modules/%s/build/' % kernel,
+             '-C', target_dir],
+            env=env)
+        proc.wait(timeout=300)
+
+        if proc.returncode != 0:
+            print('Failed to build %s, exit code: %d' % (target_mod, proc.returncode))
+            return False
+
+        if not os.path.exists(target_mod):
+            print('%s is missing.' % target_mod)
+            return False
+
+        print('Successfully built %s.' % target_mod)
+        return True
+
+    @staticmethod
+    def load_module(mod, debug=False):
+        '''Load the specified kernel module.
+
+        mod - path to .ko file.
+
+        If 'debug' is True, debug (more verbose) mode will be enabled
+        for the module. Mostly applicable to the KEDR core.
+        '''
+        print('Loading %s.' % mod)
+        cmd = ['insmod', mod]
+        if debug:
+            cmd.append('debug=1')
+        subprocess.run(cmd, check=True)
+
+    @staticmethod
+    def unload_module(mod):
+        '''Unload the specified kernel module.
+
+        mod - path to .ko file.
+        '''
+        modname, _, _ = os.path.basename(mod).rpartition('.')
+        modname = modname.replace('-', '_')
+
+        if os.path.exists('/sys/module/%s/' % modname):
+            print('Unloading "%s"' % modname)
+            subprocess.run(['rmmod', modname], check=True)
+        else:
+            print('Module "%s" is not loaded, nothing to unload.' % modname)
+
+
+    def enable_kedr(self):
+        '''Enable KEDR core.'''
+        subprocess.run(['sh', '-c', 'echo 1 > ' + KEDR_ENABLED], check=True)
+        with open('/sys/kernel/kedr/enabled', 'r') as ke:
+            line = ke.readline().strip()
+            self.assertEqual(int(line), 1)
+
+    def disable_kedr(self, check=True):
+        '''Disable KEDR core.'''
+        subprocess.run(['sh', '-c', 'echo 0 > ' + KEDR_ENABLED], check=check)
+        if check:
+            with open('/sys/kernel/kedr/enabled', 'r') as ke:
+                line = ke.readline().strip()
+                self.assertEqual(int(line), 0)
+
+    def check_dmesg_line(self, line, dmesg_file):
+        '''Look for kernel problems reported in the given line of dmesg.'''
+        if line.find(' BUG: ') != -1 or line.find(' general protection fault: ') != -1:
+            print('Got kernel BUG:\n' + line)
+            print('See %s for details.' % os.path.join(os.getcwd(), dmesg_file))
+            raise RuntimeError('Kernel BUG was found.')
+
+        if line.find(' WARNING: ') != -1:
+            print('Got kernel WARNING:\n' + line)
+            print('See %s for details.' % os.path.join(os.getcwd(), dmesg_file))
+            raise RuntimeError('Kernel WARNING was found.')
+
+    def save_dmesg_before(self):
+        '''Save dmesg in a file before the test.'''
+        dmesg_file = './dmesg-before.log'
+        subprocess.run(['sh', '-c', 'dmesg > ' + dmesg_file], check=True)
+        with open(dmesg_file, 'r') as dmesg:
+            for ln in dmesg:
+                line = ln
+        self.last_dmesg_line = line
+
+    def check_dmesg_after(self):
+        '''Check the new messages from dmesg after the test.'''
+        print('Checking dmesg after the test.')
+        dmesg_file = './dmesg-after.log'
+        subprocess.run(['sh', '-c', 'dmesg > ' + dmesg_file], check=True)
+        check = False
+        with open(dmesg_file, 'r') as dmesg:
+            for ln in dmesg:
+                if check:
+                    self.check_dmesg_line(ln.strip(), dmesg_file)
+                elif ln.startswith(self.last_dmesg_line):
+                    check = True
+        # dmesg has fully rotated during the test - check the whole dmesg.
+        # Some of it may already be lost, but checking the rest is better
+        # than nothing.
+        if not check:
+            print('Note: dmesg has fully rotated. We may miss some messages.')
+            with open(dmesg_file, 'r') as dmesg:
+                for ln in dmesg:
+                    self.check_dmesg_line(ln.strip(), dmesg_file)
+
+        print('Found no problems in dmesg.')
+
+
+class KedrTestBasics(KedrTest):
+    '''Test the basic operations with KEDR.'''
+    target_subdir = 'target'
+    target_mod = target_subdir + '/kedr_common_target.ko'
+    target_write_cmd = 'echo 1234567890 > /dev/cfake0'
+    target_read_cmd = 'dd if=/dev/cfake0 of=/dev/null bs=40 count=1'
+    target_loop_cmd = ' '.join(['while true; do',
+                                target_write_cmd, ';',
+                                target_read_cmd, '2>/dev/null;',
+                                'done'])
+
+    def __init__(self, methodName='runTest'):
+        KedrTest.__init__(self, methodName)
+
+        rules = os.path.join(self.topsrcdir, 'i13n/rules.yml')
+        if not os.path.exists(rules):
+            raise RuntimeError('File not found: %s.\n' % rules)
+
+        if os.path.exists(self.target_mod):
+            print('%s already exists, will not rebuild it.' % self.target_mod)
+        else:
+            shutil.copytree(os.path.join(self.topbuilddir, 'tests/common_target'),
+                            os.path.join(self.testdir, self.target_subdir))
+            built_ok = self.build_target_module(
+                self.target_subdir, self.target_mod, self.kernel, rules)
+            if not built_ok:
+                raise RuntimeError('Failed to build target module')
+
+    def target_do_read(self):
+        '''Read something from the file managed by the target module.'''
+        print('Trying to read from /dev/cfake0.')
+        proc = subprocess.run(['sh', '-c', self.target_read_cmd])
+        self.assertEqual(proc.returncode, 0)
+
+    def target_do_write(self):
+        '''Write something to the file managed by the target module.'''
+        print('Trying to write to /dev/cfake0.')
+        proc = subprocess.run(['sh', '-c', self.target_write_cmd])
+        self.assertEqual(proc.returncode, 0)
+
+    def target_start_rw_loop(self):
+        '''Read from / write to the file in a loop.'''
+        print('Starting to access /dev/cfake0 in an infinite loop.')
+        return subprocess.Popen(['sh', '-c', self.target_loop_cmd])
+
+    def setUp(self):
+        self.save_dmesg_before()
+        self.load_module(KEDR_CORE, debug=True)
+
+    def tearDown(self):
+        # Disable KEDR, just in case it remains enabled for some reason.
+        # The core module cannot be unloaded while KEDR is enabled.
+        self.disable_kedr(check=False)
+        self.unload_module(KEDR_CORE)
+
+        # Just in case the test failed somehow and did not unload the target
+        # module itself. It is OK to call it even, if the target is not
+        # loaded.
+        self.unload_module(self.target_mod)
+
+        print('Waiting a bit before checking dmesg...')
+        time.sleep(5)
+        self.check_dmesg_after()
+
+    def test_enable_disable(self):
+        '''Check enable/disable operations.'''
+        self.enable_kedr()
+
+        self.load_module(self.target_mod)
+
+        self.target_do_read()
+        self.target_do_write()
+
+        self.disable_kedr()
+        self.target_do_read()
+        self.target_do_write()
+
+        self.enable_kedr()
+        self.target_do_read()
+        self.target_do_write()
+
+        self.unload_module(self.target_mod)
+        self.disable_kedr()
+
+    def test_enable_disable_under_load(self):
+        '''Check enable/disable operations when the target is working.'''
+        self.load_module(self.target_mod)
+        proc = self.target_start_rw_loop()
+
+        # Wait a bit and check if the loop is actually running.
+        time.sleep(5)
+        self.assertTrue(proc.poll() is None)
+
+        # Now enable KEDR, let it monitor the target a little, then disable.
+        self.enable_kedr()
+        time.sleep(5)
+        self.disable_kedr()
+
+        # ... and do it again, to check repetitive enable/disable operations
+        # in such conditions.
+        self.enable_kedr()
+        time.sleep(5)
+        self.disable_kedr()
+
+        # Wait a bit more and check if the loop is still running.
+        time.sleep(5)
+        self.assertTrue(proc.poll() is None)
+
+        # Stop the loop.
+        stopped = False
+        proc.kill()
+        try:
+            proc.wait(timeout=10)
+            stopped = True
+        except subprocess.TimeoutExpired:
+            print('Process %d did not stop.' % proc.pid)
+
+        if not stopped:
+            # Wait longer.
+            try:
+                proc.wait(timeout=30)
+                stopped = True
+            except subprocess.TimeoutExpired:
+                print('Process %d did not stop (may be hung?).' % proc.pid)
+
+        self.assertTrue(stopped)
+        self.unload_module(self.target_mod)
+
+
+if __name__ == '__main__':
+    tests = {'basics' : KedrTestBasics}
+
+    parser = argparse.ArgumentParser(description='The tests for KEDR.')
+    parser.add_argument(
+        'test_name', choices=tests.keys(),
+        help='name of the test case to run')
+    parser.add_argument(
+        '--topsrcdir', required=True,
+        help='the top directory of the source tree')
+    parser.add_argument(
+        '--topbuilddir', required=True,
+        help='the top directory of the build tree')
+    parser.add_argument(
+        '--testdir', required=True,
+        help='directory the tests may use for their data')
+    args = parser.parse_args()
+
+    kernel = os.uname().release
+    print('Target kernel: ' + kernel)
+    sys.stdout.flush()
+
+    testdir = os.path.join(args.testdir, kernel, args.test_name)
+    if os.path.exists(testdir):
+        shutil.rmtree(testdir)
+
+    os.makedirs(testdir)
+    os.chdir(testdir)
+
+    build_kedr_core(args.topsrcdir, testdir, kernel)
+
+    testclass = tests[args.test_name]
+    testclass.kernel = kernel
+    testclass.testdir = testdir
+    testclass.topsrcdir = args.topsrcdir
+    testclass.topbuilddir = args.topbuilddir
+
+    # run the tests
+    suite = unittest.defaultTestLoader.loadTestsFromTestCase(testclass)
+    result = unittest.TextTestRunner().run(suite)
+    if result.failures or result.errors:
+        sys.exit(1)
