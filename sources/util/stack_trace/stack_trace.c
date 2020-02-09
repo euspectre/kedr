31e086a38f29505f94cde214a61186b31e1581e7
Evgenii Shatokhin <eshatokhin@virtuozzo.com>
2020-02-09 19:29:16 +0300

    core: Adapt to the changes in stack trace API from the kernel 5.2+
    Mainline commit
    e9b98e162aa5 "stacktrace: Provide helpers for common stack trace operations"
    introduced stack_trace_save(), which should be used in the kernel 5.2
    and newer, while save_stack_trace() becomes an implementation detail and
    is no longer available.
    
    Use stack_trace_save() for 5.2+ and implement it for the older kernels,
    similar to what that mainline commit does.

--- sources/util/stack_trace/stack_trace.c
+++ sources/util/stack_trace/stack_trace.c
@@ -15,30 +15,42 @@
  ======================================================================== */
 
 #include <linux/kernel.h>
+#include <linux/version.h>
 #include <linux/sched.h>
 #include <linux/stacktrace.h>
 
 #include <kedr/util/stack_trace.h>
 
-void
-kedr_save_stack_trace(unsigned long *entries, unsigned int max_entries,
-	unsigned int *nr_entries,
-	unsigned long first_entry)
+/*
+ * See the changes made to stack trace API in mainline commit
+ * e9b98e162aa5 "stacktrace: Provide helpers for common stack trace operations".
+ * Kernel 5.2+ has stack_trace_save() already.
+ */
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 2, 0)
+noinline unsigned int stack_trace_save(unsigned long *store,
+				       unsigned int size,
+				       unsigned int skipnr)
+{
+	struct stack_trace trace = {
+		.entries = store,
+		.max_entries = size,
+		.skip = skipnr + 1,
+	};
+
+	save_stack_trace(&trace);
+	return trace.nr_entries;
+}
+#endif
+
+void kedr_save_stack_trace(unsigned long *entries, unsigned int max_entries,
+			   unsigned int *nr_entries,
+			   unsigned long first_entry)
 {
 	unsigned int i = 0;
+	unsigned int nr;
 	int found = 0;
 	unsigned long stack_entries[KEDR_NUM_FRAMES_INTERNAL];
-	struct stack_trace trace = {
-		.nr_entries = 0,
-		.entries = &stack_entries[0],
-		
-		/* Request as many entries as we can. */
-		.max_entries = KEDR_NUM_FRAMES_INTERNAL,
-			
-		/* We need all frames, we'll do filtering ourselves. */
-		.skip = 0
-	}; 
-	
+
 	BUG_ON(entries == NULL);
 	BUG_ON(nr_entries == NULL);
 	BUG_ON(max_entries > KEDR_MAX_FRAMES);
@@ -48,13 +60,16 @@ kedr_save_stack_trace(unsigned long *entries, unsigned int max_entries,
 		return;
 	}
 	
-	save_stack_trace(&trace);
+	nr = stack_trace_save(
+		&stack_entries[0],
+		KEDR_NUM_FRAMES_INTERNAL, /* as many entries as we can get */
+		0 /* we need all stack entries here */);
 	
 	/* At least one entry will be stored. */
 	*nr_entries = 1;
 	entries[0] = first_entry;
 	
-	for (i = 0; i < trace.nr_entries; ++i)
+	for (i = 0; i < nr; ++i)
 	{
 		if (*nr_entries >= max_entries) break;
 
